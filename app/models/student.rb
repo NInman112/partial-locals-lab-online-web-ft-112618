43ef0159b00edf4ba57ec2c184616f08886c0d89
Nick Inman <nick@theoatmeal.com>
2019-02-16 19:35:06 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search(student_search)
+  def self.search(student_search)
     binding.pry
   end
 end
