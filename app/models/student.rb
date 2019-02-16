f7b1108f0a3641e5a2c18b304d618d7ee20eb4cc
Nick Inman <nick@theoatmeal.com>
2019-02-16 20:28:04 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,7 +15,7 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(student_search)
-    if student_search == "" || student_search.nil?ß
+    if student_search == "" || student_search.nil?
       Student.all
     else
       Student.all.select {|student| student.name.downcase.include?(name.downcase)}
